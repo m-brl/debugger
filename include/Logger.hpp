72347e74e453686dc8c31fee40b3e2bf74b980ec
m-brl <103381146+m-brl@users.noreply.github.com>
2026-04-20 14:32:26 -0400

    evol(compilation): update compilation process
    Signed-off-by: m-brl <103381146+m-brl@users.noreply.github.com>

--- include/Logger.hpp
+++ include/Logger.hpp
@@ -1,7 +1,11 @@
 #pragma once
 
+#include <ctime>
+#include <format>
+#include <iostream>
 #include <mutex>
 #include <string>
+#include <sys/time.h>
 
 #include "constant.h"
 
@@ -18,5 +22,37 @@ private:
   static void logErr(std::string message, int flags);
 
 public:
-  static void log(const char *message, int flags = 0);
+  template <typename T>
+    requires requires(T t) { (std::cout << t); }
+  static void log(T message, int flags = 0) {
+    std::string out;
+
+    if (!HAS_FLAG(flags, NO_TIME)) {
+      std::lock_guard lock(_timeMutex);
+      time_t now = {};
+      struct timeval tv = {};
+      struct tm *timeinfo = {};
+
+      time(&now);
+      gettimeofday(&tv, nullptr);
+      timeinfo = localtime(&now);
+
+      out += std::format("[{}-{:02}-{:02} {:02}:{:02}:{:02}.{:03}] ",
+                         timeinfo->tm_year + 1900, timeinfo->tm_mon + 1,
+                         timeinfo->tm_mday, timeinfo->tm_hour, timeinfo->tm_min,
+                         timeinfo->tm_sec, tv.tv_usec / 1000);
+    }
+
+    out += message;
+
+    if (HAS_FLAG(flags, OUT)) {
+      logOut(out, flags);
+    }
+    if (HAS_FLAG(flags, ERR)) {
+      logErr(out, flags);
+    }
+    if (!HAS_FLAG(flags, OUT) && !HAS_FLAG(flags, ERR)) {
+      logOut(out, flags);
+    }
+  }
 };
--- src/Logger.cpp
+++ src/Logger.cpp
@@ -1,9 +1,6 @@
 #include "Logger.hpp"
 
-#include <ctime>
-#include <format>
 #include <iostream>
-#include <sys/time.h>
 
 void Logger::logOut(std::string message, int flags) {
   std::lock_guard lock(_outMutex);
@@ -20,38 +17,6 @@ void Logger::logErr(std::string message, int flags) {
   std::cerr << message;
   if (!HAS_FLAG(flags, NO_ENDL)) {
     std::cerr << std::endl;
-  }
-}
-
-void Logger::log(const char *message, int flags) {
-  std::string out;
-
-  if (!HAS_FLAG(flags, NO_TIME)) {
-    std::lock_guard lock(_timeMutex);
-    time_t now = {};
-    struct timeval tv = {};
-    struct tm *timeinfo = {};
-
-    time(&now);
-    gettimeofday(&tv, nullptr);
-    timeinfo = localtime(&now);
-
-    out += std::format("[{}-{:02}-{:02} {:02}:{:02}:{:02}.{:03}] ",
-                       timeinfo->tm_year + 1900, timeinfo->tm_mon + 1,
-                       timeinfo->tm_mday, timeinfo->tm_hour, timeinfo->tm_min,
-                       timeinfo->tm_sec, tv.tv_usec / 1000);
-  }
-
-  out += message;
-
-  if (HAS_FLAG(flags, OUT)) {
-    logOut(out, flags);
-  }
-  if (HAS_FLAG(flags, ERR)) {
-    logErr(out, flags);
-  }
-  if (!HAS_FLAG(flags, OUT) && !HAS_FLAG(flags, ERR)) {
-    logOut(out, flags);
   }
 }
 
